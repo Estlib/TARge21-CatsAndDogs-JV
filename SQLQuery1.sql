dd224df26e72ee4207ad2926ebe79a38f703a9ed
jürivaitmaa <jyrivaitmaa@gmail.com>
2022-05-10 14:35:26 +0300

    cats and dogs 1st push

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿select Pet.Name, PetType.PetTypeName from PetType
+join Pet on Pet.TypeId = PetType.Id
+
+select Pet.Name, PetType.PetTypeName from PetType
+inner join Pet on Pet.TypeId = PetType.Id
+where PetType.PetTypeName like 'cat'
+
+select Pet.Name, PetType.PetTypeName from PetType
+inner join Pet on Pet.TypeId = PetType.Id
+where PetType.PetTypeName like 'dog'
